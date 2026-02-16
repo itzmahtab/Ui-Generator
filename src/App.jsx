8b40d679fa4b223876ba37058f6e440c83fd6726
Mahi <hero90960@gmail.com>
2026-02-16 14:25:02 +0600

    setup up vite+tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+const App = () => {
+  return (
+
+    <>
+      <BrowserRouter>
+      {/* Navigation */}
+      <nav>
+        <Link to="/">Home</Link> |{" "}
+        <Link to="/about">About</Link> |{" "}
+        <Link to="/contact">Contact</Link>
+      </nav>
+
+      {/* Routes */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+
+    </>
+  )
+}
+
+export default App
